a9c67555349232ecfc7745fb59d3c6f1882bf29c
Deserie Murembeni <deserie.dev@gmail.com>
2021-12-08 13:21:08 +0200

    initial project structure

---
+++ src/components/app/App.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+import Header from './Header.js';
+import Main from './Main.js';
+import Footer from './Footer.js';
+import api from '../utils/api';
+
+function App() {
+
+  return (
+    <Header />
+    <Main />
+    <Footer />
+  );
+}
+
+export default App;
