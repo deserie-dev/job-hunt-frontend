99852d8bae8c7ecf315654499742de310186887f
Deserie Murembeni <deserie.dev@gmail.com>
2021-12-31 18:30:52 +0200

    add scrollToTop

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,11 +5,13 @@ import Header from '../header/Header';
 import SearchForm from '../searchForm/SearchForm';
 import PageNotFound from '../pageNotFound/PageNotFound';
 import Footer from '../footer/Footer';
+import ScrollToTop from '../scrollToTop/ScrollToTop';
 
 function App() {
 
   return (
     <div className='page'>
+      <ScrollToTop />
       <Header />
       <Switch>
         <Route exact path="/">
