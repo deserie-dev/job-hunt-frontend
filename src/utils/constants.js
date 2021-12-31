79f7ee896d024ed318698bdd2e685cd08fbaad17
Deserie Murembeni <deserie.dev@gmail.com>
2021-12-31 14:04:33 +0200

    update api.js

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+const API_KEY = 'd15362807amsh21ff82b82badd18p1e0439jsn7e39c5e227ec';
+const BASE_URL = `https://awesome-indeed.p.rapidapi.com/indeed_jobs_detailed?`;
+
+module.exports = {
+  API_KEY,
+  BASE_URL,
+};
