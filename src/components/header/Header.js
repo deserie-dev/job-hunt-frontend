b27d139520b3c337b7516e61a1b70c1a05cdc0e1
Deserie Murembeni <deserie.dev@gmail.com>
2022-01-01 19:12:28 +0200

    fix Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,28 @@
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import "./Header.css";
+
 
 function Header() {
+  const [burger, setBurger] = useState(true);
+
+  const toggleDropDown = () => {
+    setBurger(!burger);
+  };
+
+  
   return(
-    <div className= "header">
-      <Link to = "/"><div className="header__logo">
-        <p>JobHunt</p>
-       </div>
-      </Link>
-    </div>
+    <header className={`header ${burger ? '' : 'header__mobile'}`}>
+      <NavLink to ="/" className="header__logo">JobHunt</NavLink>
+
+      <button onClick={toggleDropDown} type="button" className={`header__hamburger ${burger ? 'header__button-open' : 'header__button-close'}`} />
+      
+      <div id="header__links" className={`header__links ${burger ? 'header__links-hidden' : 'header__links-open'}`}>
+        <NavLink activeClassName="header__links-item_selected" className="header__links-item" to="/">Home</NavLink>
+        <NavLink activeClassName="header__links-item_selected" className="header__links-item" to="/services">Services</NavLink>
+        <NavLink activeClassName="header__links-item_selected" className="header__links-item" to="/contact">Contact</NavLink>
+      </div>      
+    </header>
   )
 }
 
