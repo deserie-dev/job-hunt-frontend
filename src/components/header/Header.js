d8301e808b8ab6b1a1ee54d956cebed7f705fc40
Deserie Murembeni <deserie.dev@gmail.com>
2022-01-18 14:24:50 +0200

    add hamburger menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import "./Header.css";
 
 function Header () {
+  const [burger, setBurger] = useState(true);
+
+  const toggleDropDown = () => {
+    setBurger(!burger);
+  };
+
   
   return(
-    <header className="header">
+    <header className={`header ${burger ? '' : 'header__mobile'}`}>
       <NavLink to ="/" className="header__logo">JobHunt</NavLink>
+
+      <button onClick={toggleDropDown} type="button" className={`header__hamburger ${burger ? 'header__button-open' : 'header__button-close'}`} />
       
-      <div className="header__links">
-        <NavLink className="header__links-item" to="/">Home</NavLink>
-        <NavLink className="header__links-item" to="/services">Services</NavLink>
+      <div id="header__links" className={`header__links ${burger ? 'header__links-hidden' : 'header__links-open'}`}>
+        <NavLink className="header__links-item" to="/services">About</NavLink>
         <NavLink className="header__links-item" to="/contact">Contact</NavLink>
       </div>      
     </header>
